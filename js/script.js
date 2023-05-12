d8aa3b033a288010ba30042ca613f3e637df37bb
Antonio Piras <antonio.piras90@gmail.com>
2023-05-12 17:03:59 +0200

    creo array di ogetti forniti

---
+++ js/script.js
@@ -0,0 +1,32 @@
+// creo array di ogettiv forniti 
+const infoUtenti = [
+
+    {
+    "nome e  cognome":"Wayne Barnett",
+    "professione":"Founder & CEO"
+    },
+{
+    "nome e  cognome":"Angela Carrol",
+    "professione":"Chief Editor"
+    },
+
+{
+    "nome e  cognome":"Walter Gordon",
+    "professione":"Office Manager"
+    },
+{
+    "nome e  cognome":"Angela Lopez",
+    "professione":"Social Media Manager"
+    },
+{
+    "nome e  cognome":"Scott Estrada",
+    "professione":"Developer"
+    },
+{
+    "nome e  cognome":"Barbara Ramos",
+    "professione":"Graphic Designer"
+    },
+
+
+];
+console.log(infoUtenti);
