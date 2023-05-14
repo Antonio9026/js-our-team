bfbdd77df28a58c8b2093d46f39ad453169183d4
Antonio Piras <antonio.piras90@gmail.com>
2023-05-14 14:35:18 +0200

    Stampo in console le card senza css

--- js/script.js
+++ js/script.js
@@ -2,31 +2,59 @@
 const infoUtenti = [
 
     {
-    "nome e  cognome":"Wayne Barnett",
-    "professione":"Founder & CEO"
+        "nome": "Wayne Barnett",
+        "professione": "Founder & CEO",
+         "foto": "wayne-barnett-founder-ceo.jpg"
     },
-{
-    "nome e  cognome":"Angela Carrol",
-    "professione":"Chief Editor"
-    },
-
-{
-    "nome e  cognome":"Walter Gordon",
-    "professione":"Office Manager"
-    },
-{
-    "nome e  cognome":"Angela Lopez",
-    "professione":"Social Media Manager"
-    },
-{
-    "nome e  cognome":"Scott Estrada",
-    "professione":"Developer"
-    },
-{
-    "nome e  cognome":"Barbara Ramos",
-    "professione":"Graphic Designer"
+    {
+        "nome": "Angela Carrol",
+        "professione": "Chief Editor",
+        "foto":"angela-caroll-chief-editor.jpg"
     },
 
+    {
+        "nome": "Walter Gordon",
+        "professione": "Office Manager",
+         "foto":"walter-gordon-office-manager.jpg"
+    },
+    {
+        "nome": "Angela Lopez",
+        "professione": "Social Media Manager",
+        "foto":"angela-lopez-social-media-manager.jpg"
+    },
+    {
+        "nome": "Scott Estrada",
+        "professione": "Developer",
+        "foto":"scott-estrada-developer.jpg"
+    },
+    {
+        "nome": "Barbara Ramos",
+        "professione": "Graphic Designer",
+        "foto":"barbara-ramos-graphic-designer.jpg"
+    },
 
-];
-console.log(infoUtenti);
+
+]
+// seleziono il container dove creare le mie card
+const teamContainer = document.querySelector('.card_container');
+
+// richiamo la funziomne per stampare la card 
+createInfo(infoUtenti, teamContainer);
+
+// creo la funzione per stampare la card
+
+function createInfo(infoUtenti, teamContainer) {
+
+    
+// creo ciclo per stampare ogni oggetto
+
+    for (let i = 0; i < infoUtenti.length; i++) {
+        const teamCard = infoUtenti[i];
+    // compongo l'elemento da stapare
+        teamContainer.innerHTML +=
+            `<div class="team-card"><div class="card-image"><img class = "img_container"src="img/${teamCard.foto}" /></div><divclass="card-text"><p>${teamCard.nome}</p><div><p>${teamCard.professione}</p></div></div></div>`;
+    }
+}
+
+
+
